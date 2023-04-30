4afa63dd0d644cfe02d4527222f6c3c0bbff96bd
Andrei Chubarev <a-chubarev@mail.ru>
2023-04-30 19:06:53 +0600

    add main file

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+import unittest
 
-
-driver = webdriver.Chrome()
-time.sleep(10)
-
-driver.get('https://suninjuly.github.io/text_input_task.html')
-find_text =  driver.find_element(By.CSS_SELECTOR, '.textarea')
-
-find_text.send_keys("get()")
-time.sleep(15)
-
-submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
-
-submit_button.click()
-time.sleep(10)
-
-
-driver.quit()
+if __name__ == "__main__":
+    unittest.main()
